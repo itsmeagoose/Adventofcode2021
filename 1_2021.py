655e8678ca4d45d8f2be3672fd5950fcdd810feb
itsmeagoose <91782508+itsmeagoose@users.noreply.github.com>
2021-12-01 11:43:54 +0000

    Add files via upload

---
+++ 1_2021.py
@@ -0,0 +1,23 @@
+puzzel = open('2021day1.txt','r').read().splitlines()
+m = 0
+n = 0
+a = 0
+b = 1
+c = 2
+d = 3
+count = 0
+run = False
+increses = 0
+while d < len(puzzel):
+    m = (int(puzzel[a]) + int(puzzel[b]) + int(puzzel[c]))
+    n = (int(puzzel[b]) + int(puzzel[c]) + int(puzzel[d]))
+    if n > m:
+        increses += 1
+        #print(m, n)
+    else:
+        print(m, n)
+    a += 1
+    b += 1
+    c += 1
+    d += 1
+print(increses)
